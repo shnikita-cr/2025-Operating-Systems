692f1049411dcb7a11b4709fc44e408d09a5e727
Nikita <professornikson@gmail.com>
2025-10-28 23:31:06 +0300

    add: create test folders script

---
+++ lab1/create_test_folders.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+mkdir -p test_folder/a
+mkdir -p test_folder/b
+mkdir -p test_folder/c
+mkdir -p test_folder/d
+mkdir -p test_folder/e
+
+touch test_folder/a/file0.txt
+touch test_folder/a/file1.txt
+
+touch test_folder/b/file0.txt
+touch test_folder/b/file1.txt
+
+touch test_folder/c/file0.txt
+touch test_folder/c/file1.txt
+
+touch test_folder/d/file0.txt
+
+touch test_folder/e/file0.txt
